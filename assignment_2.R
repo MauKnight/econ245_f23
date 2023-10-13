d73f928fbd943bdef2c240952503fbd11f7a4deb
ZacharySOlsen <zs0lsenprice@gmail.com>
2023-10-13 16:33:15 -0700

    Data Wrangling Homework 2

--- assignment_2.R
+++ assignment_2.R
@@ -6,8 +6,23 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 library(tidyverse) # importing a package
 
 #------ Uploading PERMID --------------------------------#
-PERMID <- "9911876" #Type your PERMID within the quotation marks
+PERMID <- "7205065" #Type your PERMID within the quotation marks
 PERMID <- as.numeric(gsub("\\D", "", PERMID)) #Don't touch
 set.seed(PERMID) #Don't touch
 
 #------ Answer ------------------------------------------#
+airbnb = read_csv("airbnb_data.csv")
+
+airbnb = rename(airbnb, neighborhood = neighbourhood)
+
+airbnb %>% count(neighborhood) %>% filter(!is.na(neighborhood)) %>% arrange() %>% head(n = 20)
+
+neighborhoods = airbnb %>% count(neighborhood) %>% filter(!is.na(neighborhood)) %>% arrange(desc(n)) %>% head(n = 20)
+
+airbnb_top_neighborhoods = airbnb %>% filter(neighborhood %in% neighborhoods$neighborhood)
+
+summary_stats_top_neighborhoods = airbnb_top_neighborhoods %>% group_by(neighborhood) %>% summarize(avg_square_feet = mean(square_feet, na.rm = TRUE), avg_price = mean(price, na.rm = TRUE), sd_price = sd(price, na.rm =T), max_price = max(price, na.rm =T), min_price = min(price, na.rm =T)) %>% arrange(desc(avg_square_feet))
+
+highest_avg_square_ft = summary_stats_top_neighborhoods %>% slice(1) %>% pull(avg_square_feet)
+
+second_avg_price = summary_stats_top_neighborhoods %>% arrange(desc(avg_price)) %>% slice(2) %>% pull(avg_price)
