7673cf9927070873316796589078323395a47685
MauKnight <nmcolladoduran@gmail.com>
2023-10-13 13:37:29 -0700

    Push to Mauricio Branch

--- assignment_2.R
+++ assignment_2.R
@@ -11,3 +11,45 @@ PERMID <- as.numeric(gsub("\\D", "", PERMID)) #Don't touch
 set.seed(PERMID) #Don't touch
 
 #------ Answer ------------------------------------------#
+
+## Problem 2
+
+# P2 1. Setting data
+airbnb <- read_csv("airbnb_data.csv") %>% # read csv
+  rename("neighborhood"="neighbourhood")
+
+# P2 2.a
+neighborhoods <- airbnb %>% 
+  filter(!is.na(neighborhood)) %>% 
+  group_by(neighborhood) %>% 
+  summarize(n=n()) %>% 
+  arrange(desc(n)) %>%
+  head(20) %>% 
+  ungroup()
+
+
+# P2 2.b
+airbnb_top_neighborhoods <- airbnb %>% 
+  filter(neighborhood %in% neighborhoods$neighborhood)
+
+# P2 2.c
+summary_stats_top_neighborhoods <- airbnb_top_neighborhoods %>% 
+  group_by(neighborhood) %>% 
+  summarize(avg_square_feet=mean(square_feet, na.rm = TRUE),
+            avg_price=mean(price, na.rm = TRUE),
+            sd_price=sd(price,na.rm = TRUE),
+            max_price=max(price, na.rm = TRUE),
+            min_price=min(price, na.rm = TRUE)) %>% 
+  arrange(desc(avg_square_feet))
+
+# P2 2.d
+highest_avg_square_ft <- summary_stats_top_neighborhoods %>% 
+  slice(1) %>% 
+  pull(avg_square_feet)
+
+# P2 2.e
+second_avg_price <- summary_stats_top_neighborhoods %>% 
+  slice(2) %>% 
+  pull(avg_price)
+
+#------ End ------------------------------------------#
